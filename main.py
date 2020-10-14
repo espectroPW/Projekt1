95603070a6ec75dfde4a0fd1ad4d7661ec7ecbf2
espectroPW <admin@espectro.pw>
2020-10-14 23:37:50 +0200

    Podstawy

--- main.py
+++ main.py
@@ -11,20 +11,28 @@ typy = open('australian.txt').read()
 typy = [item.split() for item in typy.split('\n')]
 typypd = pd.DataFrame(data=typy)
 
-
-
 print(np.matrix(dane))
+
+
+# A[1] <- drugi wiersz
+# A[1][2] <- 3 element w 2 wierszu
+# A[Y][X]  <-
 
 print("#############################")
 
+wiersze = len(dane[:][:])
+
+kolumny = len(dane[0][:])
+
+print("Wiersze: {}. Kolumny: {}".format(wiersze, kolumny))
+
 x = 0
 
-while True:
-    print(np.matrix(dane[x][0]))
-    x = x+1
 
+def wyswietl(tablica, x, y):
 
-
+    for x in range(0, wiersze):
+        print(np.matrix(dane[0][x]))
 
 
 
